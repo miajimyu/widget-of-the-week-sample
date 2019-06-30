1d9078414bc6213908be452bc049b8b0e006132b
miajimyu <38499336+miajimyu@users.noreply.github.com>
2019-06-30 22:51:42 +0900

    Add Positioned

---
+++ positioned/README.md
@@ -0,0 +1,13 @@
+# Positioned
+
+## Docs
+
+[Positioned class](https://api.flutter.dev/flutter/widgets/Positioned-class.html)
+
+[Layout widgets](https://flutter.dev/docs/development/ui/widgets/layout)
+
+## Screenshots
+
+|Menu|Sample1|
+|:-:|:-:|
+|<img src="./screenshots/Menu.png" height="400" alt="Screenshot"/>|<img src="./screenshots/Sample1.png" height="400" alt="Screenshot"/>|
---
+++ positioned/lib/pages/sample1.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Sample1Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sample1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue[50],
+                ),
+                Positioned(
+                  top: 25,
+                  left: 50,
+                  child: FlutterLogo(),
+                )
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue[50],
+                ),
+                Positioned(
+                  bottom: 50,
+                  right: 25,
+                  child: FlutterLogo(),
+                )
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue[50],
+                ),
+                Positioned(
+                  bottom: 50,
+                  right: 25,
+                  height: 75,
+                  width: 75,
+                  child: FlutterLogo(),
+                )
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue[50],
+                ),
+                Positioned.fill(
+                  child: FlutterLogo(),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
