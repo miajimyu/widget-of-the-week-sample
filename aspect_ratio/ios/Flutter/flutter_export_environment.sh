ecbb3edd53b540efcd173b3d3421d3641bad12ae
miajimyu <38499336+miajimyu@users.noreply.github.com>
2019-11-14 21:58:11 +0900

    chore: Update Dart

---
+++ aspect_ratio/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/miajimyu/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/miajimyu/Desktop/widget-of-the-week-sample/aspect_ratio"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/miajimyu/Desktop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
