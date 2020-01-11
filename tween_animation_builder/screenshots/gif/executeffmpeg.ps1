4167a9fa748a783509b8665624dc4a927d12e4a5
miajimyu <38499336+miajimyu@users.noreply.github.com>
2020-01-11 15:51:39 +0900

    [TweenAnimationBuilder] Update gif

--- tween_animation_builder/screenshots/gif/executeffmpeg.ps1
+++ tween_animation_builder/screenshots/gif/executeffmpeg.ps1
@@ -4,7 +4,7 @@ $files = Get-ChildItem *.mov
 
 foreach ($file in $files) {
   ffmpeg -i $file.name -an -r 10 %04d.png
-  convert *.png -resize 40% output_%04d.png
+  convert *.png -resize 80% output_%04d.png
   convert output_*.png "$($file.BaseName).gif"
   Remove-Item *.png
 }
