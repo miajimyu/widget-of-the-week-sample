5a2b053fe7d87e64854ca196aa650e7e6a8ebbd4
miajimyu <38499336+miajimyu@users.noreply.github.com>
2020-01-18 00:03:38 +0900

    [FractionallySizedBox] Add analysis_options.yaml

--- fractionally_sized_box/lib/pages/sample2.dart
+++ fractionally_sized_box/lib/pages/sample2.dart
@@ -5,27 +5,27 @@ class Sample2Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sample2'),
+        title: const Text('Sample2'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Win a Dash',
               style: TextStyle(
                 color: Colors.lightBlue,
                 fontSize: 30,
               ),
             ),
-            Flexible(
+            const Flexible(
               child: FractionallySizedBox(
                 heightFactor: 0.1,
               ),
             ),
             Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
                 color: Colors.lightBlue,
               ),
               width: 200,
